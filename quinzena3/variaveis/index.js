3840f7e2a208dd90b6adfe5433eadcc65e762c34
Andréa Soares <asoaresdev@gmail.com>
2020-09-15 20:58:14 -0300

    Atividade referente à aula de variáveis

---
+++ quinzena3/variaveis/index.js
@@ -0,0 +1,57 @@
+// 10
+// 10 5
+// 10 10 10
+
+/*
+let nome 
+let idade
+console.log(typeof(nome));
+console.log(typeof(idade));
+*/
+
+// A variável foi decladrada só com seu nome, sem sua atribuição
+
+/*
+let nome = prompt("Qual seu nome?")
+let idade = prompt("Qual sua idade?")
+console.log(typeof(nome));
+console.log(typeof(idade));
+// retornou string nos dois casos porque o prompt retorna string
+*/
+
+/*
+let nome = prompt("Qual seu nome?")
+let idade = prompt("Qual sua idade?")
+console.log("ola",nome,", você tem",idade, "anos");
+*/
+
+/*
+let comida = prompt("Qual sua comida preferida?")
+let local = prompt("Próximas férias, irá para aonde??")
+let carro = prompt("Qual seu carro dos sonhos?")
+let cerveja = prompt("Qual sua cerveja preferida?")
+let time = prompt("Qual time você torce?")
+console.log("1-Qual sua comida preferida??\n Resposta:" ,comida);
+console.log("2-Próximas férias, irá para aonde??\n Resposta:" ,local);
+console.log("3-Qual seu carro dos sonhos?\n Resposta:" ,carro);
+console.log("4-Qual sua cerveja preferida?\n Resposta:" ,cerveja);
+console.log("5-Qual time você torce??\n Resposta:" ,time);
+*/
+
+/*
+let comidaFavorita = ["pizza", "churrasco", "batata frita", "estrogonofe", "cheetos"]
+console.log(comidaFavorita);
+console.log("Essas são minhas comidas favoritas:\n" ,comidaFavorita[0], "\n" ,comidaFavorita[1],"\n",comidaFavorita[2],"\n",comidaFavorita[3],"\n",comidaFavorita[4]);
+
+let preferida = prompt("qual sua comida favorita?")
+comidaFavorita[1] = preferida
+console.log("Essas são minhas comidas favoritas:\n" ,comidaFavorita[0], "\n" ,comidaFavorita[1],"\n",comidaFavorita[2],"\n",comidaFavorita[3],"\n",comidaFavorita[4]);
+*/
+
+/*
+let perguntas = ["você tem uma casa?","você tem um carro?","você tem uma moto?"]
+let respostas = [true, true, false]
+console.log(perguntas[0], respostas[0])
+console.log(perguntas[1], respostas[1])
+console.log(perguntas[2], respostas[2])
+*/
