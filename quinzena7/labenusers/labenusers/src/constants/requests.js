20d9e276ed2941b4b113b1e67f26518413cc18ee
Andréa Soares <asoaresdev@gmail.com>
2020-11-18 12:24:19 -0300

    resolvendo untrack do exerccício da aula

---
+++ quinzena7/labenusers/labenusers/src/constants/requests.js
@@ -0,0 +1,7 @@
+export const BASE_URL = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
+
+export const axiosConfig = {
+    headers: {
+      Authorization: "andrea-soares-tang"
+    }
+  };
